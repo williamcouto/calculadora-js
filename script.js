fe7f088258b92b1dfa7cc4a241bbfb7af3e5ff2b
williamcouto <willplacido40@gmail.com>
2024-10-21 14:22:58 -0300

    add script on backspace button

--- script.js
+++ script.js
@@ -11,29 +11,12 @@ function clearNums(){
 }
 
 // Função para apgar o ultimo caractere
-// Capturar os números que o usuario coloca
-// Colocar os numeros em um array
-// Quando o botão DEL for clicado, ele remove o ultimo ultimo numero
-
 function deleteLastNum(){
-    let bttns_nums = document.querySelectorAll('.num-bttn')
-    let bttn_Del = document.getElementById('delBttn')
-    let num_captured = []
+    let display = document.querySelector('.display_result')
+    let displayAtual = display.textContent
 
-    // Capturando os numeros
-        bttns_nums.forEach(button => {
-            button.addEventListener('click', () => {
-                // Obtem o valor númerico do botão clicado
-                const number = button.textContent
-                //Adiciona o número ao array de números clicados - usar o push()
-                num_captured.push(number)
-                let nums_result = num_captured.join('')
-                document.querySelector('.display_result').textContent = num_captured.join('')
-            })
-        });
-
-        bttn_Del.addEventListener('click', () => {
-            num_captured.pop()
-
-        })
+    if(displayAtual.length > 0){
+        let displayNums = displayAtual.substring(0, displayAtual.length - 1)
+        display.textContent = displayNums
+    } 
 }
