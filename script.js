5dafe91a3ea8caac0c92e1e6707b7cc25486f06c
williamcouto <willplacido40@gmail.com>
2024-11-14 20:51:09 -0300

    Removendo as funções e adicionando classes

--- script.js
+++ script.js
@@ -1,22 +1,20 @@
-let bttns_nums = document.getElementsByClassName('num-bttn')
+let bttnNum = document.querySelectorAll('[data-num-btn]')
+let operatorBttn = document.querySelectorAll('[data-operator-btn]')
+let currentDisplay = document.querySelector('[data-display]')
+let equalBtn = document.querySelectorAll('[data-operator-equal]')
+let deleteBtn = document.querySelector('[data-delete-btn]')
+let resetBtn = document.querySelector('[data-reset-btn]')
 
-// Inserindo numeros no display
-function insertDisplayNums(num){
-    document.querySelector('.display_result').textContent += num
-}
+class Calculator {
+    constructor(equalBtn, display, bttns_nums, operatorBttn){
+        this.equalBtn = equalBtn
+        this.display = display
+        this.bttns_nums = bttns_nums
+        this.operatorBttn = operatorBttn
+        this.iniciarCalc()
+    }
 
-// Limpando o Display
-function clearNums(){
-    document.querySelector('.display_result').textContent = ""
-}
-
-// Função para apgar o ultimo caractere
-function deleteLastNum(){
-    let display = document.querySelector('.display_result')
-    let displayAtual = display.textContent
-
-    if(displayAtual.length > 0){
-        let displayNums = displayAtual.substring(0, displayAtual.length - 1)
-        display.textContent = displayNums
-    } 
+    clearNums(){
+        this.display.textContent = ""
+    }
 }
