c33fbcef06c43153b3fb7cc43e82f8c1b60def1a
williamcouto <willplacido40@gmail.com>
2024-11-20 20:58:59 -0300

    Adicionando instâncias para testar calculadora

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let bttnNum = document.querySelectorAll('[data-num-btn]')
+let bttnNumbers = document.querySelectorAll('[data-num-btn]')
 let operatorBttn = document.querySelectorAll('[data-operator-btn]')
 let currentDisplay = document.querySelector('[data-display]')
 let equalBtn = document.querySelectorAll('[data-operator-equal]')
@@ -6,16 +6,18 @@ let deleteBtn = document.querySelector('[data-delete-btn]')
 let resetBtn = document.querySelector('[data-reset-btn]')
 
 class Calculator {
-    constructor(equalBtn, currentDisplay, bttns_nums, operatorBttn){
+    constructor(equalBtn, currentDisplay, bttnNumbers, operatorBttn, deleteBtn, resetBtn){
         this.equalBtn = equalBtn
         this.currentDisplay = currentDisplay
-        this.bttns_nums = bttns_nums
+        this.bttnNumbers = bttnNumbers
         this.operatorBttn = operatorBttn
+        this.deleteBtn = deleteBtn
+        this.resetBtn = resetBtn
         this.iniciarCalc()
     }
 
     iniciarCalc(){
-        this.bttns_nums.forEach(button => {
+        this.bttnNumbers.forEach(button => {
             button.addEventListener('click', () => {
                 this.insertNum(button.textContent)
             })
@@ -30,5 +32,16 @@ class Calculator {
         this.currentDisplay.textContent += num
     }
 
-
+    deleteLastNum(){
+        if(this.currentDisplay.textContent.length > 0){
+            this.currentDisplay.textContent = this.currentDisplay.textContent.slice(0, -1)
+        }
+    }
 }
+
+// Testando calculadora
+const calc = new Calculator(equalBtn, currentDisplay, bttnNumbers, operatorBttn, deleteBtn, resetBtn)
+
+resetBtn.addEventListener('click', () => {
+    calc.clearNums()
+})
